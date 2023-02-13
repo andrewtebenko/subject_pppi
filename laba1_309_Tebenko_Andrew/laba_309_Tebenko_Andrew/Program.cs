96e7d982616057f6b0f39118f8362a849755b912
Andrew Tebenko <101203262+andrewtebenko@users.noreply.github.com>
2023-02-13 14:44:26 +0200

    Upload first job files

---
+++ laba1_309_Tebenko_Andrew/laba_309_Tebenko_Andrew/Program.cs
@@ -0,0 +1,64 @@
+﻿new ConsoleApp().Run();
+
+class ConsoleApp
+{
+    private string selectMenu;
+
+    private void GetUserData()
+    {
+        Console.WriteLine("Please, select any menu item");
+
+        Console.WriteLine("1: Write some words of the text 'Lorem ipsum'");
+
+        Console.WriteLine("2: Calculate a mathematical operation");
+
+        selectMenu = Console.ReadLine();
+    }
+
+    private void CollectDataFromInput()
+    {
+        switch (selectMenu)
+        {
+            case "1":
+                {
+                    Console.WriteLine(ReadData());
+                }
+                break;
+            case "2":
+                {
+                    Console.WriteLine(MultiplyData());
+                }
+                break;
+            default:
+                {
+                    Console.WriteLine("You do something wrong!");
+                }
+                break;
+        }
+    }
+
+    private string ReadData()
+    {
+        Console.WriteLine("Enter number of words");
+
+        int numberOfWords = Int32.Parse(Console.ReadLine());
+
+        StreamReader streamReader =
+            new StreamReader("C:\\Users\\Acer\\source\\repos\\ConsoleApp1\\ConsoleApp1\\data.txt");
+        
+        string[] words = streamReader.ReadLine().Split(" ");
+        
+        return string.Join(" ", words.Take(numberOfWords));
+    }
+
+    private int MultiplyData()
+    {
+        return 10 * 3;
+    }
+
+    public void Run()
+    {
+        GetUserData();
+        CollectDataFromInput();
+    }
+}
