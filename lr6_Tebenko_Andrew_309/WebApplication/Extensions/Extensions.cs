348e92dddd2b8205cbcf711b3d179a2a2a58a3bc
Andrew Tebenko <101203262+andrewtebenko@users.noreply.github.com>
2023-04-30 18:37:04 +0300

    Upload sixth job files

---
+++ lr6_Tebenko_Andrew_309/WebApplication/Extensions/Extensions.cs
@@ -0,0 +1,1 @@
+// This empty file I created that I can upload to GIT
---
+++ lr6_Tebenko_Andrew_309/WebApplication/Project.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace WebApplication;
+
+public class Project
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("version 1", new OpenApiInfo { Title = "First project - Tebenko Andrew", Version = "version 1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "First project - Tebenko Andrew");
+            c.DocExpansion(DocExpansion.None);
+        });
+    }    
+}
